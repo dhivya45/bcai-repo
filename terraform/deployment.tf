68ce0e6eb2e6b17f0ad76d7b2d71fe42b1842a6f
dhivya45 <84711785+dhivya45@users.noreply.github.com>
2022-02-21 11:19:59 +0530

    Update deployment.tf

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,10 +1,10 @@
 ##Configuring Terraform Backend
 terraform {   
  backend "azurerm" {
- resource_group_name   = bcai-test-poc
- storage_account_name  = bcaipocstg
- container_name        = tfback
- key                   = rs.tf
+ resource_group_name   = "var.resource_group_name1"
+ storage_account_name  = "var.storage"
+ container_name        = "var.container_name"
+ key                   = "var.key_name"
  }
 }
 resource "azurerm_resource_group" "example" {
