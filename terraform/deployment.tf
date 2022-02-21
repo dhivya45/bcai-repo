cf681dd09839b08aaaca1d7139836d9f36fea498
dhivya45 <84711785+dhivya45@users.noreply.github.com>
2022-02-21 12:43:17 +0530

    Update deployment.tf

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,10 +1,6 @@
 ##Configuring Terraform Backend
 terraform {   
  backend "azurerm" {
- resource_group_name   = "var.resource_group_name1"
- storage_account_name  = "var.storage"
- container_name        = "var.container_name"
- key                   = "var.key_name"
  }
 }
 resource "azurerm_resource_group" "example" {
