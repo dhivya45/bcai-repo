8eda1e8c287a65da97d236f26c7a5a61c2bd17ee
dhivya45 <84711785+dhivya45@users.noreply.github.com>
2022-02-21 19:06:54 +0530

    Update deployment.tf

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -3,7 +3,12 @@ terraform {
  backend "azurerm" {
  }
 }
+provider "azurerm"
+{
+ features {}
+}
 resource "azurerm_resource_group" "example" {
   name     = "var.resource_group_name"
   location = "West Europe"
 }
+
